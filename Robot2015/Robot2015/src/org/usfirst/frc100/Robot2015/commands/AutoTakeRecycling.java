6179255186ead661e3584e5880a105a61cbcaf85
Paul <superduperpacman42@gmail.com>
2015-02-06 16:29:31 -0800

    Fixed Auto Command Group

--- Robot2015/Robot2015/src/org/usfirst/frc100/Robot2015/commands/AutoTakeRecycling.java
+++ Robot2015/Robot2015/src/org/usfirst/frc100/Robot2015/commands/AutoTakeRecycling.java
@@ -11,27 +11,12 @@ public class AutoTakeRecycling extends CommandGroup {
 	private final double DRIVE_LENGTH;
 	
     public  AutoTakeRecycling() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will1 run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
     	DRIVE_LENGTH = Preferences.getDouble("AutoTakeRecycling_DriveLength");
     	SLIDE_DISTANCE = Preferences.getDouble("AutoTakeRecycling_SlideDistance");
     	
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	addSequential(new AutoSlideToLine());
     	addParallel(new AutoFollowLine(DRIVE_LENGTH));
-    	addSequential(new AutoGrabRecycling(3, 2));
+    	addSequential(new AutoGrabRecycling(3, 0));
     	addSequential(new AutoDrive(0, SLIDE_DISTANCE));
     }
 }
