314fa7b3933c08d57aa5d9e7d32a18fd84957f23
ArgareVarg <fredrik@landin.us>
2015-02-08 15:44:09 -0800

    Revert "hopefully fixing  a lot of things"
    This reverts commit d172bde44011fe55706be220d99aca454c9d424c.

--- Ian/NetTables Client/src/netTablesClient/Client.java
+++ Ian/NetTables Client/src/netTablesClient/Client.java
@@ -1,8 +1,9 @@
-package netTablesClient; 
-import java.io.BufferedWriter; 
-import java.io.File; 
-import java.io.FileWriter; 
-import java.io.IOException; 
+package netTablesClient;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
