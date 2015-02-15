e43d3d241e7d8ace841866104c3e80f8834011aa
Definately not Paul <code@argvarg.net>
2015-02-14 16:27:49 -0800

    REALLY fixing arm

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/AutoGrabRecycling.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/AutoGrabRecycling.java
@@ -10,51 +10,36 @@ import edu.wpi.first.wpilibj.command.Command;
  * Triggers the auto arm when a container is detected.
  */
 public class AutoGrabRecycling extends Command {
-	private final int containers;
-	private final double TIME_AFTER_INITIAL_DEPLOY;
+	private final double TIME_AFTER_RETRACT;
 	private final double TIME_AFTER_STAB;
 	private final double TIME_AFTER_DEPLOY;
-	private int containersTaken = 0;
-	private boolean takeRecyclingThisTime = false;
 
 	/**
 	 * @param containers - The number of containers to take before exit
 	 * @param time - The time to wait before starting
 	 */
-	public AutoGrabRecycling(int containers) {
-		TIME_AFTER_INITIAL_DEPLOY = Preferences
-				.getDouble("AutoGrabRecycling_TIME_AFTER_1ST_DEPLOY");
-		TIME_AFTER_STAB = Preferences
-				.getDouble("AutoGrabRecycling_TIME_AFTER_STAB");
+	public AutoGrabRecycling() {
 		TIME_AFTER_DEPLOY = Preferences
-				.getDouble("AutoGrabRecycling_TIME_AFTER_2ND_DEPLOY");
-		this.containers = containers;
+				.getDouble("AutoGrabRecycling_TIME_AFTER_DEPLOY");
+		TIME_AFTER_STAB = Preferences
+				.getDouble("AutoGrabRecycling_TIME_AFTER_GRAB");
+		TIME_AFTER_RETRACT = Preferences
+				.getDouble("AutoGrabRecycling_TIME_AFTER_RETRACT");
 	}
 
 	public void initialize() {
 		SlideWinder.arm.setDeploy(true);
-		Timer.delay(TIME_AFTER_INITIAL_DEPLOY);
+		Timer.delay(TIME_AFTER_DEPLOY);
 		SlideWinder.arm.setStab(true);
 		Timer.delay(TIME_AFTER_STAB);
 		SlideWinder.arm.setDeploy(false);
-		Timer.delay(TIME_AFTER_DEPLOY);
+		Timer.delay(TIME_AFTER_RETRACT);
 		SlideWinder.arm.setStab(false);
 		SlideWinder.arm.setDeploy(true);
 	}
 
 	public void execute() {
-		if (SlideWinder.arm.getContainer()) {
-			if (takeRecyclingThisTime) {
-				SlideWinder.arm.setStab(true);
-				Timer.delay(TIME_AFTER_STAB);
-				SlideWinder.arm.setDeploy(false);
-				Timer.delay(TIME_AFTER_DEPLOY);
-				SlideWinder.arm.setStab(false);
-				SlideWinder.arm.setDeploy(true);
-				containersTaken++;
-			}
-			takeRecyclingThisTime = !takeRecyclingThisTime;
-		}
+		
 	}
 
 	public void end() {
@@ -62,7 +47,7 @@ public class AutoGrabRecycling extends Command {
 	}
 
 	public boolean isFinished() {
-		return containers == containersTaken;
+		return true;
 	}
 
 	@Override
