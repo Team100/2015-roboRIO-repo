8969d899c3a1f663428e6b69e854cb474976ecdb
Paul <superduperpacman42@gmail.com>
2015-02-16 10:18:52 -0800

    maybe fixing Koosha's mess

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/RobotMap.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/RobotMap.java
@@ -101,11 +101,17 @@ public class RobotMap {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 //        elevatorBrake = new DoubleSolenoid(0, 2, 3);      
 //        LiveWindow.addActuator("Elevator", "Brake", elevatorBrake);
         
 =======
 >>>>>>> origin/master
+=======
+        elevatorBrake = new DoubleSolenoid(0, 2, 3);      
+        LiveWindow.addActuator("Elevator", "Brake", elevatorBrake);
+        
+>>>>>>> parent of b5091a2... Put DeployPiston in the correct position & removed ElevatorBrake
 =======
         elevatorBrake = new DoubleSolenoid(0, 2, 3);      
         LiveWindow.addActuator("Elevator", "Brake", elevatorBrake);
@@ -143,10 +149,14 @@ public class RobotMap {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         armDeployPiston = new DoubleSolenoid(0, 0, 1);      
 =======
         armDeployPiston = new DoubleSolenoid(0, 2, 3);      
 >>>>>>> origin/master
+=======
+        armDeployPiston = new DoubleSolenoid(1, 0, 1);      
+>>>>>>> parent of b5091a2... Put DeployPiston in the correct position & removed ElevatorBrake
 =======
         armDeployPiston = new DoubleSolenoid(1, 0, 1);      
 >>>>>>> parent of b5091a2... Put DeployPiston in the correct position & removed ElevatorBrake
