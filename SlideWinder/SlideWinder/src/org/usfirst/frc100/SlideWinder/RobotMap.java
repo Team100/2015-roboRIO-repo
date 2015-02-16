c01023396767aefe0edaefbb45e715c033d9a535
Paul Nadan <kooshajahani@yahoo.com>
2015-02-15 20:41:59 -0800

    Revert "Removed Elevator Brake from"
    This reverts commit 80d838a055dbc837a4b89749507192eb582f811d.

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/RobotMap.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/RobotMap.java
@@ -26,6 +26,7 @@ public class RobotMap {
     public static AnalogInput drivetrainRightLineReader;
     public static SpeedController elevatorMotor;
     public static Encoder elevatorEncoder;
+    public static DoubleSolenoid elevatorBrake;
     public static DigitalInput elevatorUpperLimit;
     public static DigitalInput elevatorLowerLimit;
     public static SpeedController armRaiseMotor;
@@ -97,6 +98,9 @@ public class RobotMap {
         LiveWindow.addSensor("Elevator", "Encoder", elevatorEncoder);
         elevatorEncoder.setDistancePerPulse(1.0);
         elevatorEncoder.setPIDSourceParameter(PIDSourceParameter.kRate);
+        elevatorBrake = new DoubleSolenoid(0, 2, 3);      
+        LiveWindow.addActuator("Elevator", "Brake", elevatorBrake);
+        
         elevatorUpperLimit = new DigitalInput(8);
         LiveWindow.addSensor("Elevator", "UpperLimit", elevatorUpperLimit);
         
@@ -121,7 +125,7 @@ public class RobotMap {
         armBackLimit = new DigitalInput(12);
         LiveWindow.addSensor("Arm", "BackLimit", armBackLimit);
         
-        armDeployPiston = new DoubleSolenoid(0, 2, 3);      
+        armDeployPiston = new DoubleSolenoid(1, 0, 1);      
         LiveWindow.addActuator("Arm", "DeployPiston", armDeployPiston);
         
         pneumaticsCompressor = new Compressor(0);
