cc7382dbb3f270a246cf18725ff356caa8d066a8
theEnchantress <anamtudor@gmail.com>
2015-02-16 10:56:27 -0800

    QuickStack CommandGroups added
    Now using joystick buttons 11 and 12

---
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/QuickStackOne.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc100.SlideWinder.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * Places a stack on one tote and picks up the whole stack
+ */
+public class QuickStackOne extends CommandGroup {
+
+	public QuickStackOne() {
+		addSequential(new SetElevatorPosition(2));
+		addSequential(new OpenClaw());
+		addSequential(new SetElevatorPosition(1));
+		addSequential(new CloseClaw());
+	}
+
+}
---
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/QuickStackTwo.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc100.SlideWinder.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+/**
+ * Places a stack on two totes and picks up the whole stack
+ */
+public class QuickStackTwo extends CommandGroup {
+
+	public QuickStackTwo() {
+		addSequential(new SetElevatorPosition(3));
+		addSequential(new OpenClaw());
+		addSequential(new SetElevatorPosition(1));
+		addSequential(new CloseClaw());
+	}
+
+}
