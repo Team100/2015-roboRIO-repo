6b62a21d9212094694666baa5382e22f7973272d
Paul <superduperpacman42@gmail.com>
2015-02-19 18:45:27 -0800

    Preferences ready for testing

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/Preferences.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/Preferences.java
@@ -1,18 +1,14 @@
 package org.usfirst.frc100.SlideWinder;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
-
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.ITableListener;
 
 /**
- * Stores all changeable or arbitrary values in a file on the cRIO.
+ * Stores all changeable or arbitrary values in a file on the cRIO, as well as a
+ * temporary copy in NetworkTables.
  */
 public class Preferences {
 
@@ -23,46 +19,46 @@ public class Preferences {
 	/**
 	 * Sets a preference value, creating the preference if it does not exist
 	 * 
-	 * @param name - The name of the preference
+	 * @param key - The name of the preference
 	 * @param value - The desired value of the preference
 	 */
-	public static void set(String name, Object value) {
-		table.putString(name, value+"");
-		if (!keys.contains(name)) keys.add(name);
+	public static void set(String key, Object value) {
+		table.putString(key, value+"");
+		if (!keys.contains(key)) keys.add(key);
 	}
 
 	/**
 	 * Casts double to an object for regular set method
 	 * 
-	 * @param name - The name of the preference
+	 * @param key - The name of the preference
 	 * @param value - The desired value of the preference
 	 */
-	public static void set(String name, double value) {
-		set(name, Double.valueOf(value));
+	public static void set(String key, double value) {
+		set(key, Double.valueOf(value));
 	}
 
 	/**
 	 * Casts boolean to an object for regular set method
 	 * 
-	 * @param name - The name of the preference
+	 * @param key - The name of the preference
 	 * @param value - The desired value of the preference
 	 */
-	public static void set(String name, boolean value) {
-		set(name, Boolean.valueOf(value));
+	public static void set(String key, boolean value) {
+		set(key, Boolean.valueOf(value));
 	}
 
 	/**
 	 * Returns value of a preference as a double
 	 * 
-	 * @param name - The name of the preference
+	 * @param key - The name of the preference
 	 * @return The value of the preference
 	 */
-	public static double getDouble(String name) {
-		String value = getString(name);
+	public static double getDouble(String key) {
+		String value = getString(key);
 		try {
 			return Double.parseDouble(value);
 		} catch (NumberFormatException e) {
-			System.err.println("Preference is not a double: " + name + DIVIDER + value);
+			System.err.println("Preference is not a double: " + key + DIVIDER + value);
 			return 0;
 		}
 	}
@@ -70,13 +66,13 @@ public class Preferences {
 	/**
 	 * Returns value of a preference as a boolean
 	 * 
-	 * @param name - The name of the preference
+	 * @param key - The name of the preference
 	 * @return The value of the preference
 	 */
-	public static boolean getBoolean(String name) {
-		String value = getString(name);
+	public static boolean getBoolean(String key) {
+		String value = getString(key);
 		if (!(value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false"))) {
-			System.err.println("Preference is not a boolean: " + name + DIVIDER + value);
+			System.err.println("Preference is not a boolean: " + key + DIVIDER + value);
 		}
 		return value.equalsIgnoreCase("true");
 	}
@@ -84,15 +80,15 @@ public class Preferences {
 	/**
 	 * Returns value of a preference as a String
 	 * 
-	 * @param name - The name of the preference
+	 * @param key - The name of the preference
 	 * @return The value of the preference
 	 */
-	public static String getString(String name) {
-		if (table.containsKey(name)) {
-			return table.getString(name);
+	public static String getString(String key) {
+		if (table.containsKey(key)) {
+			return table.getString(key);
 		} else {
-			System.err.println("Preference not found: " + name);
-			set(name, "0");
+			System.err.println("Preference not found: " + key);
+			set(key, "0");
 			return "0";
 		}
 	}
@@ -100,11 +96,11 @@ public class Preferences {
 	/**
 	 * Returns whether a preference exists
 	 * 
-	 * @param name - The name of the preference
+	 * @param key - The name of the preference
 	 * @return The existence of the given preference
 	 */
-	public static boolean contains(String name) {
-		return table.containsKey(name);
+	public static boolean contains(String key) {
+		return table.containsKey(key);
 	}
 
 	/**
@@ -115,16 +111,16 @@ public class Preferences {
 		try {
 			in = new BufferedReader(new FileReader("/home/lvuser/Preferences.txt"));
 			keys.clear();
-			for (int i = 0; i < 10000; i++) { // End of file
+			for (int i = 0; i < 10000; i++) {
 				String line = in.readLine();
-				if (line == null) {
+				if (line == null) { // End of file
 					break;
 				} else if(line.length() == 0) { // Newline
 					keys.add("");
 				} else if (line.contains(DIVIDER)) { // Key and value
 					int dividerIndex = line.indexOf(DIVIDER);
 					String key = line.substring(0, dividerIndex);
-					String value = line.substring(dividerIndex + DIVIDER.length(), line.length());
+					String value = line.substring(dividerIndex + DIVIDER.length());
 					set(key, value);
 				} else { // Key without value
 					set(line, "0");
@@ -147,9 +143,10 @@ public class Preferences {
 		try {
 			out = new BufferedWriter(new FileWriter("/home/lvuser/Preferences.txt"));
 			for (int i = 0; i < keys.size(); i++) {
-				if(){
+				if(!keys.get(i).equals("")){ // Empty String indicates a blank line
 					String line = keys.get(i) + DIVIDER + table.getString(keys.get(i));
-				out.write(line);
+					out.write(line);
+				}
 				out.newLine();
 			}
 			out.flush();
@@ -167,6 +164,7 @@ public class Preferences {
 		table.addTableListener(new ITableListener() {
 			@Override
 			public void valueChanged(ITable source, String key, Object value, boolean isNew) {
+				// Make sure the code knows all the keys
 				if(!keys.contains(key)) keys.add(key);
 			}
 		}, true);
