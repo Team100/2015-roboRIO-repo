96213f1adfc31e33971896c8c28125ab6433c0ee
theEnchantress <anamtudor@gmail.com>
2015-02-21 11:07:20 -0800

    AutoModeFive: Pull bin back (get out of way)

---
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/AutoModeFive_PullBinBack.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc100.SlideWinder.commands;
+
+import org.usfirst.frc100.SlideWinder.SlideWinder;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * Places a stack on one tote and picks up the whole stack
+ */
+public class AutoModeFive_PullBinBack extends CommandGroup {
+
+	public AutoModeFive_PullBinBack() {
+		if( SlideWinder.claw.isClosed() ){
+			addSequential(new OpenClaw());
+			addSequential(new AutoDrive(12));
+		} 
+		addSequential(new CloseClaw());
+		addSequential(new SetElevatorPosition(2));
+		addSequential(new AutoDrive(-108));
+		addSequential(new SetElevatorPosition(1));
+		addSequential(new OpenClaw());
+	}
+
+}
