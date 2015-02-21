d27c6db39bd0e9119f65ac8b26dadd41417784eb
theEnchantress <anamtudor@gmail.com>
2015-02-21 12:09:55 -0800

    AutoModeSix: Push bin and tote forward

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/AutoModeSix_PushBinAndTote.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/AutoModeSix_PushBinAndTote.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc100.SlideWinder.commands;
 
+import org.usfirst.frc100.SlideWinder.Preferences;
 import org.usfirst.frc100.SlideWinder.SlideWinder;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -7,17 +8,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 /**
  * Places a stack on one tote and picks up the whole stack
  */
-public class AutoModeFive_PullBinBack extends CommandGroup {
+public class AutoModeSix_PushBinAndTote extends CommandGroup {
 
-	public AutoModeFive_PullBinBack() {
+	public AutoModeSix_PushBinAndTote() {
 		addSequential(new AutoCalibrateElevator());
-		if( SlideWinder.claw.isClosed() ){
-			addSequential(new OpenClaw());
-			addSequential(new AutoDrive(12));
-		} 
+		addSequential(new OpenClaw());
+		addSequential(new AutoDrive(Preferences.getDouble("AutoStackTotes_DistanceForward"), 0));
 		addSequential(new CloseClaw());
 		addSequential(new SetElevatorPosition(2));
-		addSequential(new AutoDrive(-108));
+		addSequential(new AutoDrive(-12, 0));
+		addSequential(new AutoDrive(0, 20));
+		addSequential(new AutoDrive(120));
 		addSequential(new SetElevatorPosition(1));
 		addSequential(new OpenClaw());
 	}
