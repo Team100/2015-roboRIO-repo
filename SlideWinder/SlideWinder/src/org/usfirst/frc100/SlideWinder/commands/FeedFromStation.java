14d6727a71b3b5a787f382dfa3c45b48c0fc370a
Paul Nadan <kooshajahani@yahoo.com>
2015-02-21 16:57:50 -0800

    Finished it and added comments

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/FeedFromStation.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/FeedFromStation.java
@@ -2,9 +2,16 @@ package org.usfirst.frc100.SlideWinder.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
+/**
+ * 
+ * @author Student
+ * The driver will park in front of the human player station and add the tote ejected to the bottom of a held stack
+ */
 public class FeedFromStation extends CommandGroup {
 
 	public FeedFromStation() {
+		addSequential(new SetElevatorPosition(24.0, false));	// high enough to unblock the tote shoot
+		addSequential(new AutoDelay(2.0));
 		addSequential(new SetElevatorPosition(2));
 		addSequential(new OpenClaw());
 		addSequential(new SetElevatorPosition(1));
