a24de6e69173b155fafbc0bc1572b544653f1e42
Team100Cien <superduperpacman42@gmail.com>
2015-02-28 10:54:59 -0800

    Ana's Changes

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/SlideWinder.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/SlideWinder.java
@@ -75,15 +75,19 @@ public class SlideWinder extends IterativeRobot {
 				new AutoModeFour_TakeRecyclingWithLineReaders().start();
 				break;
 			case 5:
-				new AutoModeFive_PullBinBack().start();
+				new AutoModeFive_PullBinBack(1).start();
 				break;
 			case 6:
-				new AutoModeSix_PushBinAndTote().start();
+				new AutoSlideToLine().start();
 				break;
 			case 7:
-				new AutoModeSeven_LandmarkStack().start();
+				new AutoVisionFollowLine(48).start();
+				break;
+			case 8:
+				new AutoFollowLine(48).start();
 				break;
 			default:
+				new Immobilize().start();
 				break;
 		}
         new UpdateDashboard().start();
