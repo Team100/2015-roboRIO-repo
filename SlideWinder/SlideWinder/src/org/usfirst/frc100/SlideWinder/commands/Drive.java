a1f1c7836c11b69b75df04f30d33f0a6c226b3ff
Paul Nadan <kooshajahani@yahoo.com>
2015-03-07 16:46:10 -0800

    Moved the QuickStack commands to the left and right D-Pad
    Cleaned up OI.java and fixed ManipulatorManualControl.java so the arm
    won't constantly toggle.

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/Drive.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/commands/Drive.java
@@ -19,14 +19,15 @@ public class Drive extends Command {
 	}
 
 	// Called just before this Command runs the first time
+	@Override
 	protected void initialize() {
 	}
 
 	// Called repeatedly when this Command is scheduled to run
+	@Override
 	protected void execute() {
 		double leftJoystickX = SlideWinder.oi.getLeftJoystick().getX();
-		if (Math.abs(leftJoystickX) < Preferences
-				.getDouble("SlideJoystickDeadband")) {
+		if (Math.abs(leftJoystickX) < Preferences.getDouble("SlideJoystickDeadband")) {
 			leftJoystickX = 0;
 		}
 		// Joystick Y-axis values are flipped, so send a negative Y-value
@@ -35,16 +36,19 @@ public class Drive extends Command {
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
+	@Override
 	protected boolean isFinished() {
 		return false;
 	}
 
 	// Called once after isFinished returns true
+	@Override
 	protected void end() {
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
+	@Override
 	protected void interrupted() {
 	}
 }
