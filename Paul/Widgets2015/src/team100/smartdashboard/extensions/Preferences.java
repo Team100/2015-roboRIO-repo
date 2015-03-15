cac7b160a7484506af5eeec347a43e23de7ca748
Team100Cien <superduperpacman42@gmail.com>
2015-03-15 15:46:45 -0700

    Revert "Preference widget actually fixed"
    This reverts commit f3b75d1347c0cb14a53b2e869b84f278dc774b3a.

--- Paul/Widgets2015/src/team100/smartdashboard/extensions/Preferences.java
+++ Paul/Widgets2015/src/team100/smartdashboard/extensions/Preferences.java
@@ -1,7 +1,6 @@
 package team100.smartdashboard.extensions;
 
 import edu.wpi.first.smartdashboard.gui.StaticWidget;
-import edu.wpi.first.smartdashboard.gui.Widget;
 import edu.wpi.first.smartdashboard.properties.Property;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
@@ -11,8 +10,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.util.ArrayList;
-import java.util.Arrays;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 
@@ -23,19 +20,15 @@ import javax.swing.JTextField;
 public class Preferences extends StaticWidget {
 
     public static final String NAME = "Preference Viewer";
-    private JComboBox keyBox = new JComboBox();
+    private final JComboBox keyBox = new JComboBox();
     private final JTextField valueField = new JTextField();
     private final ITable prefs = NetworkTable.getTable("Preferences");
-    private ArrayList<Object> labels = new ArrayList<>();
-    private ArrayList<Widget.EditorTextField> fields = new ArrayList<>();
 
     @Override
     public void init() {
         setPreferredSize(new Dimension(415, 40));
         keyBox.setPreferredSize(new Dimension(200, 25));
         valueField.setPreferredSize(new Dimension(200, 25));
-        
-        keyBox.addItem("Generate Preference");
 
         // If the user changes the JComboBox, update the value field
         keyBox.addActionListener(new ActionListener() {
@@ -51,12 +44,7 @@ public class Preferences extends StaticWidget {
         valueField.addFocusListener(new FocusAdapter() {
             @Override
             public void focusLost(FocusEvent e) {
-                if(keyBox.getSelectedItem().equals("Generate Preference") && !valueField.getText().equals("") 
-                        && !prefs.containsKey(valueField.getText())){
-                    prefs.putString(valueField.getText(), "");
-                } else {
-                    prefs.putString(keyBox.getSelectedItem() + "", valueField.getText());
-                }
+                prefs.putString(keyBox.getSelectedItem() + "", valueField.getText());
             }
         });
 
@@ -65,10 +53,8 @@ public class Preferences extends StaticWidget {
         prefs.addTableListener(new ITableListener() {
             @Override
             public void valueChanged(ITable itable, String key, Object value, boolean isNew) {
-                if (isNew && !key.equals("Generate Preference")) {
-                    System.out.println("Added pref");
+                if (isNew) {
                     keyBox.addItem(key);
-                    sort();
                 }
                 if (key.equals(keyBox.getSelectedItem())) {
                     valueField.setText(value + "");
@@ -82,23 +68,5 @@ public class Preferences extends StaticWidget {
 
     @Override
     public void propertyChanged(Property prprt) {
-    }
-    
-    //Organizes the JComboBox display alphabetically
-    public void sort() {
-        System.out.println("sort");
-        int size = keyBox.getItemCount() - 1;
-        Object[] temp = new String[size];
-        for(int i = 0; i < size; i++) {
-            temp[i] = keyBox.getItemAt(i+1);
-        }      
-        Arrays.sort(temp);
-        
-        keyBox.removeAllItems();
-        keyBox.addItem("Generate Preference");
-        for(int i = 0; i < size; i++){
-            keyBox.addItem(temp[i]);
-        }
-        
     }
 }
