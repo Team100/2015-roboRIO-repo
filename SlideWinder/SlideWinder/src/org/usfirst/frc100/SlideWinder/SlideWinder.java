fc075c0e69bb23b14ef74b130b97657296d51588
Team100Cien <superduperpacman42@gmail.com>
2015-03-15 15:47:15 -0700

    Revert "Can now pull multiple bins back"
    This reverts commit a61cf17e78d08d1d4e0403ec897eba687287dd2b.

--- SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/SlideWinder.java
+++ SlideWinder/SlideWinder/src/org/usfirst/frc100/SlideWinder/SlideWinder.java
@@ -75,16 +75,13 @@ public class SlideWinder extends IterativeRobot {
 				new AutoModeFour_TakeRecyclingWithLineReaders().start();
 				break;
 			case 5:
-				new AutoModeFive_PullBinBack(1).start();
+				new AutoFollowLine(48).start();
 				break;
 			case 6:
 				new AutoSlideToLine().start();
 				break;
 			case 7:
 				new AutoVisionFollowLine(48).start();
-				break;
-			case 8:
-				new AutoFollowLine(48).start();
 				break;
 			default:
 				new Immobilize().start();
